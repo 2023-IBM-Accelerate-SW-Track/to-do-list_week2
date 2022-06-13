31ccc2f141fb349dfc9d533e3b72cc67c21d1511
Kareithdyce <Kareith.dyce@gmail.com>
2022-06-13 16:01:00 -0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent} from '@testing-library/react';
 import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 
@@ -20,14 +20,55 @@ test('test that App component renders', () => {
   render(<App />, container);
  });
 
-test('test that new-item-button is a button', () => {
-  render(<App/>, container);
-  const element = screen.getByTestId('new-item-button');
-  expect(element.innerHTML.toLowerCase().includes("button")).toBe(true)
-});
+ test('test that App component renders Task', () => {
+  render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i})
+  const element = screen.getByRole('button', {name: /Add/i}) ;
+  fireEvent.change(inputTask, { target: { value: "History Test"}})
+  fireEvent.click(element)
+  const date = Date().toLocaleString('en-US').split(" ",4).join(' ');
+  const checkDate = screen.getByText(new RegExp(date, "i"))
+  expect(checkDate).toBeInTheDocument();
+  const check = screen.getByText(/History Test/i)
+  expect(check).toBeInTheDocument();
+ });
 
-test('test that new-item-input is an input ', () => {
-  render(<App/>, container);
-  const element = screen.getByTestId('new-item-input');
-  expect(element.innerHTML.toLowerCase().includes("input")).toBe(true)
-});
+
+ test('test that App component doesn\'t render dupicate Task', () => {
+  render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i})
+  const element = screen.getByRole('button', {name: /Add/i}) ;
+  fireEvent.change(inputTask, { target: { value: "History Test"}})
+  fireEvent.click(element)
+  const date = Date().toLocaleString('en-US').split(" ",4).join(' ');
+  const checkDate = screen.getByText(new RegExp(date, "i"))
+  expect(checkDate).toBeInTheDocument();
+  fireEvent.change(inputTask, { target: { value: "History Test"}})
+  fireEvent.click(element)
+  const check = screen.getAllByText(/Test/i)
+  expect(check.length).toBe(1);
+ });
+
+ test('test that App component doesn\'t add a blank task', () => {
+  render(<App />);
+  const element = screen.getByRole('button', {name: /Add/i}) ;
+  fireEvent.click(element)
+  const check = screen.getByText(/You have no todo's left/i)
+  expect(check).toBeInTheDocument();
+ });
+
+ test('test that App component can be deleted thru checkbox', () => {
+    render(<App />);
+    const inputTask = screen.getByRole('textbox', {name: /Add New Item/i})
+    const element = screen.getByRole('button', {name: /Add/i}) ;
+    fireEvent.change(inputTask, { target: { value: "History Test"}})
+    fireEvent.click(element)
+    const date = Date().toLocaleString('en-US').split(" ",4).join(' ');
+    const checkDate = screen.getByText(new RegExp(date, "i"))
+    expect(checkDate).toBeInTheDocument();
+    const checkTask = screen.getByRole('checkbox')
+    fireEvent.click(checkTask)
+    const check = screen.getByText(/You have no todo's left/i)
+    expect(check).toBeInTheDocument();
+   });
+  
